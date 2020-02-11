e00f654b67011009e691cc0e28ce27c22539e1b5
Sara Santana <saraa.beatrizz@gmail.com>
2020-02-10 22:19:11 -0300

    Implemented Add and Delete to database

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, url_for
+import os
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///list.db'
@@ -8,7 +10,7 @@ db = SQLAlchemy(app)
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    content = db.Column(db.String(200), nullable = False)
+    content = db.Column(db.String(120), nullable = False)
     completed = db.Column(db.Integer, default = 0)
     creationDate = db.Column(db.DateTime, default = datetime.utcnow)
 
@@ -19,7 +21,29 @@ class Todo(db.Model):
 
 @app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template('index.html')
+    if request.method == "POST":
+        task = Todo(content=request.form['content'])
+        
+        try:
+            db.session.add(task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            print("Error.")
+    else:
+        tasks = Todo.query.all()
+        return render_template('index.html', tasks=tasks)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    taskDelete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(taskDelete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return "Unable to delete task"
 
 if __name__ == "__main__":
     app.run(debug=True)
